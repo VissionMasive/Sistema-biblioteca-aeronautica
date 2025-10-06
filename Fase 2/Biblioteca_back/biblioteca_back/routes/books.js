f93310ddd3c4af1c7c8006dae6c310103f1335ee
VissionMasive <148828751+VissionMasive@users.noreply.github.com>
2025-10-06 00:28:57 -0300

    Add files via upload

--- Fase 2/Biblioteca_back/biblioteca_back/routes/books.js
+++ Fase 2/Biblioteca_back/biblioteca_back/routes/books.js
@@ -1,6 +1,7 @@
 const express= require ("express")
 const router = express.Router()
 const db = require("../db")
+const { prestarLibro, devovlerLibro } = require("../servicios/libreria")
 
 router.get("/",async (req,res,next)=>{
     try{
@@ -14,5 +15,19 @@ router.post("/",async (req,res,next)=>{
         const{rows}= await db.query("INSERT INTO public.libros (nombre, descripcion) values ($1,$2)",[req.body.nombre,req.body.descripcion])
         res.status(201)
     } catch(err) {next(err)}
+})
+router.post("/:libroid/prestar/:userid",async(req,res)=>{
+    const{libroid,userid}=req.params
+    try{
+        const result = await prestarLibro(userid,libroid)
+        res.json(result)
+    }catch(err){res.status(400).json({error:err.message})}
+})
+router.post("/:libroid/devolver/:userid",async(req,res)=>{
+    const{libroid,userid}=req.params
+    try{
+        const result = await devovlerLibro(userid,libroid)
+        res.json(result)
+    }catch(err){res.status(400).json({error:err.message})}
 })
 module.exports = router
