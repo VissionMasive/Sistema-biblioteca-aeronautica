135b0ba46b625a0fd0b07414e1ccb0323182ac83
VissionMasive <148828751+VissionMasive@users.noreply.github.com>
2025-10-16 17:47:09 -0300

    Delete Fase 2/V3/biblioteca-eta-frontend directory

--- Fase 2/V3/biblioteca-eta-frontend/src/pages/Reglamentacion.jsx
+++
@@ -1,7 +0,0 @@
-
-import React from 'react'
-import docs from '../data/reglamentacion.json'
-import DocList from '../components/DocList.jsx'
-export default function Reglamentacion(){
-  return <DocList title='Reglamentación' description='Leyes y normas externas aplicables al quehacer educativo.' docs={docs} />
-}
