2f12062890fb1b2a97f87e73cd14aaaf37a6a311
VissionMasive <148828751+VissionMasive@users.noreply.github.com>
2025-10-16 17:16:39 -0300

    Delete Fase 2/front_v2 directory

--- Fase 2/front_v2/src/api/categoryApi.jsx
+++
@@ -1,7 +0,0 @@
-const API_URL = "http://localhost:3001/api/categories";
-
-export const getCategories = async () => {
-  const res = await fetch(API_URL);
-  if (!res.ok) throw new Error(`Error ${res.status}: ${res.statusText}`);
-  return res.json();
-};
