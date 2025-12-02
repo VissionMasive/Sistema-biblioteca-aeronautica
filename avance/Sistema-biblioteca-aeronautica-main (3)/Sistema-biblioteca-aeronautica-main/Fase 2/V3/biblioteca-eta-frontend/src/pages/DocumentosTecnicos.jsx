738b47a370990307d236fa0b4f40eb295c99e92d
VissionMasive <148828751+VissionMasive@users.noreply.github.com>
2025-12-02 13:28:35 -0300

    Delete avance/Sistema-biblioteca-aeronautica-main (3)/Sistema-biblioteca-aeronautica-main/Fase 2/V3 directory

--- avance/Sistema-biblioteca-aeronautica-main (3)/Sistema-biblioteca-aeronautica-main/Fase 2/V3/biblioteca-eta-frontend/src/pages/DocumentosTecnicos.jsx
+++
@@ -1,7 +0,0 @@
-
-import React from 'react'
-import docs from '../data/documentos_tecnicos.json'
-import DocList from '../components/DocList.jsx'
-export default function DocumentosTecnicos(){
-  return <DocList title='Documentos Técnicos' description='Resoluciones, calendarios e instrumentos institucionales.' docs={docs} />
-}
